c7751c37c5e9c445c6fac952778f146dc9f18073
Yuta Kinjo <kinjoyuta.23@gmail.com>
2026-02-16 11:41:00 +0900

    疎通テスト

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-// Pub/Sub push用
 app.post("/push", (req, res) => {
-  console.log("PubSub message:", JSON.stringify(req.body));
-
-  // Pub/Subは200系を返さないと再送し続けます
-  res.status(204).send();
-});
-
-// 動作確認用
-app.get("/", (req, res) => {
-  res.send("OK");
+  console.log("Received:", req.body);
+  res.status(200).send("ok");
 });
 
 const port = process.env.PORT || 8080;
