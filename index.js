08f120571396bb28c3642895e27ca1aca326f1b6
Yuta Kinjo <kinjoyuta.23@gmail.com>
2026-02-16 10:54:52 +0900

    add push endpoint

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.post("/", (req, res) => {
-  console.log("Received:", req.body);
-  res.status(200).send("ok");
+// Pub/Sub push用
+app.post("/push", (req, res) => {
+  console.log("PubSub message:", JSON.stringify(req.body));
+
+  // Pub/Subは200系を返さないと再送し続けます
+  res.status(204).send();
+});
+
+// 動作確認用
+app.get("/", (req, res) => {
+  res.send("OK");
 });
 
 const port = process.env.PORT || 8080;
